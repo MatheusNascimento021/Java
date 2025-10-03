4ee85986cedde270803f2a6f6891b37091eb1241
Matheus Nascimento <matheus.figueiredo445@gmail.com>
2025-10-03 18:21:51 +0100

    atribuições

---
+++ exercícios/Atribuicao2.java
@@ -0,0 +1,16 @@
+
+
+package com.mycompany.atribuicao2;
+
+public class Atribuicao2 {
+
+    public static void main(String[] args) {
+        int a, b;
+        double resultado;
+        
+        a = 5;
+        b = 2;
+        resultado = (double) a / b ;
+        System.out.println(resultado);
+    }
+}

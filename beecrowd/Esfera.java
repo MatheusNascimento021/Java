42562a2215f96e7d759a03b6579ac30a4ae31f58
Matheus Nascimento <matheus.figueiredo445@gmail.com>
2025-10-06 16:26:45 +0100

    exercícios beecrowd

--- beecrowd/Esfera.java
+++ beecrowd/Esfera.java
@@ -1,13 +1,15 @@
 
-
-package com.mycompany.main;
+package com.mycompany.esfera;
 import java.util.Scanner;
 import java.util.Locale;
-public class Main {
 
+public class Esfera {
+  
     public static void main(String[] args) {
+     
         Scanner sc = new Scanner (System.in);
         Locale.setDefault(Locale.US);
+        
         double volume = sc.nextDouble();
         double raio = (4.0/3) * 3.14159 * Math.pow(volume,3) ;
         
@@ -16,4 +18,6 @@ public class Main {
         sc.close();
         
     }
+
+    
 }

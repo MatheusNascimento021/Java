42af842ce67c3599db2f93136c224a5dd9f61fed
Matheus Nascimento <matheus.figueiredo445@gmail.com>
2025-10-09 18:30:59 +0100

    Estruturas Condicionais

---
+++ exercícios/CondicionalEncadeada.java
@@ -0,0 +1,30 @@
+
+
+package com.mycompany.condicionalencadeada;
+import java.util.Scanner;
+
+public class CondicionalEncadeada {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int horas;
+        
+        System.out.println("Quantas horas?");
+        horas = sc.nextInt();
+        
+        if (horas < 12){
+            System.out.println("Bom dia!");
+            
+        }
+        else if (horas < 18){
+            System.out.println("Boa tarde!");
+        }
+        else {
+            System.out.println("Boa noite!");
+        }
+    
+       sc.close();
+    }
+}
---
+++ exercícios/CondicionalSimples.java
@@ -0,0 +1,21 @@
+
+
+package com.mycompany.condicionalsimples;
+
+
+public class CondicionalSimples {
+
+    public static void main(String[] args) {
+        
+        int x = 5;
+        
+        System.out.println("Bom dia!");
+        
+        if (x < 0){
+            System.out.println("Boa tarde!");
+            
+        }
+        
+        System.out.println("Boa noite!");
+    }
+}

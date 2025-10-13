2995ded1215e9ed5cc556e405119aa4f301d8ae9
Matheus Nascimento <matheus.figueiredo445@gmail.com>
2025-10-13 10:39:28 +0100

    switch case

---
+++ exercícios/DiaDasemana.java
@@ -0,0 +1,44 @@
+
+
+package com.mycompany.diadasemana;
+import java.util.Scanner;
+public class DiaDasemana {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        String dia;
+        
+        switch(x){
+        case 1:
+             dia = "DOMINGO";
+            break;
+        case 2:
+            dia = "SEGUNDA-FEIRA";
+            break;
+        case 3:
+            dia = "TERÇA-FEIRA";
+            break;
+        case 4:
+            dia = "QUARTA-FEIRA";
+            break;
+        case 5:
+            dia = "QUINTA-FEIRA";
+            break;
+        case 6:
+            dia = "SEXTA-FEIRA";
+            break;
+        case 7:
+            dia = "SÁBADO";
+            break;
+        default:
+            dia = "VALOR INVÁLIDO";
+            break;
+        }
+        
+        System.out.println("DIA DA SEMANA: " + dia);
+        
+        sc.close();
+    }
+}

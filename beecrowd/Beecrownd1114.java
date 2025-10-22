75a4bfa61d78a5d26930a8c4dc45d6ff662ce409
Matheus Nascimento <matheus.figueiredo445@gmail.com>
2025-10-22 13:40:11 +0100

    praticando while

---
+++ beecrowd/Beecrownd1114.java
@@ -0,0 +1,27 @@
+
+
+package com.mycompany.beecrownd1114;
+
+import java.util.Scanner;
+
+public class Beecrownd1114 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int senha = sc.nextInt();
+        
+        while (senha != 2002){
+            
+            if (senha != 2002){
+                System.out.println("Senha Invalida");
+            }
+           
+            senha = sc.nextInt();
+        }
+        System.out.println("Acesso Permitido");
+        
+        sc.close();
+    }
+}

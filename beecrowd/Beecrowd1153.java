e1ed6088a3a4c36af79afa3b9df9640a25556ea6
Matheus Nascimento <matheus.figueiredo445@gmail.com>
2025-10-28 12:36:59 +0000

    praticando

---
+++ beecrowd/Beecrowd1153.java
@@ -0,0 +1,25 @@
+
+
+package com.mycompany.beecrowd1153;
+
+import java.util.Scanner;
+
+public class Beecrowd1153 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int fatorial = 1;
+        
+        for(int i=1; i<=n; i++){
+            fatorial*=i;
+           
+        }
+        
+        System.out.println(fatorial);
+        
+        sc.close();
+    }
+}

ebcb3789b1d2074862bb0200ea649f9e17461e00
Matheus Nascimento <matheus.figueiredo445@gmail.com>
2025-11-26 18:20:47 +0000

    praticando

---
+++ beecrowd/Beecrowd1098.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.beecrowd1098;
+
+
+public class Beecrowd1098 {
+
+    public static void main(String[] args) {
+        
+        int i = 1;
+        int j = 7;
+        
+        while(i <= 9){
+            System.out.println("I="+i+" J="+j);
+            System.out.println("I="+i+" J="+(j-1));
+            System.out.println("I="+i+" J="+(j-2));
+            
+            i+=2;
+            j+=2;
+            
+            
+        }
+    }
+}
